d35efe0f4db5a83fe3502e89e7197da54d58d849
Peter Kofler <peter.kofler@code-cop.org>
2016-10-23 22:24:50 +0200

    Green sample test.

---
+++ Java/src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void shouldSample() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
