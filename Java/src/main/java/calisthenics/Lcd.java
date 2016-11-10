6794db750400e95d5f02c6c063c124555e0fcf3d
Peter Kofler <peter.kofler@code-cop.org>
2016-11-10 19:19:57 +0100

    Ignore PMD because of public API.

--- Java/src/main/java/calisthenics/Lcd.java
+++ Java/src/main/java/calisthenics/Lcd.java
@@ -2,7 +2,7 @@ package calisthenics;
 
 public class Lcd {
 
-    public String format(int number) {
+    public String format(int number) { // NOPMD - Primitive Obsession is public API
         return "   \n"
              + "  |\n"
              + "   \n"
