e24fb18280394ab3cf473adc2727b6715a70d019
Peter Kofler <peter.kofler@code-cop.org>
2016-11-10 20:02:03 +0100

    PMD/Calisthenics: ignore wrong positive.

--- Java/src/main/java/calisthenics/Size.java
+++ Java/src/main/java/calisthenics/Size.java
@@ -7,8 +7,9 @@ public class Size {
 
     private int value;
 
-    public static Size defaultSize() {
-        return new Size(1);
+    public static Size defaultSize() { // NOPMD factory method is no getter
+        return new Size(1); 
+        // TODO new Size(1) is ok, is not this.x or simply x.
     }
 
     public Size(int value) {
