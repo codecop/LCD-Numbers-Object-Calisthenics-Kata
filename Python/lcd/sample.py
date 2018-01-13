24b95e7f3ed45459f98b8b7401e4c3c28fe3b9d2
Peter Kofler <peter.kofler@code-cop.org>
2018-01-13 17:53:59 +0100

    Format.

--- Python/lcd/sample.py
+++ Python/lcd/sample.py
@@ -1,7 +1,6 @@
-class Sample:
-
-    def withElse(self):
-        if (self.a == self.b):
-            self.a = self.thirdField
-        else:
-            self.b = self.thirdField
+class Sample(object):
+    def three_fields(self):
+        self.third_field = 0
+        self.b = self.third_field
+        if self.a == self.b:
+            self.a = self.third_field
--- Python/test/SampleTest.py
+++ Python/test/SampleTest.py
@@ -1,9 +1,7 @@
 import unittest
-from lcd.sample import Sample
 
 
 class SampleTest(unittest.TestCase):
-
     def testOne(self):
         self.assertEqual(2, 1 + 1)
 
