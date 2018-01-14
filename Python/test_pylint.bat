99edb0154fadf07e9a8536e0ef78e742e26033da
Peter Kofler <peter.kofler@code-cop.org>
2018-01-14 11:45:02 +0100

    Use Pythonpath for testing pylint itself.

--- Python/test_pylint.bat
+++ Python/test_pylint.bat
@@ -1,1 +1,4 @@
-pylint pylint\samples\sample.py --load-plugins two-instance-variables,no-else
+@setlocal
+@set PYTHONPATH=pylint;%PYTHONPATH%
+call pylint pylint\samples\sample.py --load-plugins two-instance-variables,no-else
+@endlocal
