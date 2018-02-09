4acc37f4867893301e7fcd6bf2bd7eca35820a83
Peter Kofler <peter.kofler@code-cop.org>
2018-02-09 22:50:07 +0100

    Move Test script into pylint to avoid confision of participants.

--- Python/test_pylint.bat
+++
@@ -1,5 +0,0 @@
-@setlocal
-@set PYTHONPATH=pylint;%PYTHONPATH%
-call pylint --rcfile objectcalisthenics.pylintrc pylint\samples\sample.py
-@rem ---jobs 4 --load-plugins two-instance-variables,no-else
-@endlocal
